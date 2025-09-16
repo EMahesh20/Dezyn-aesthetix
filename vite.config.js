58a05af29e41b6c5e790db1904992264e6eecd4e
EMahesh20 <maheshelango20@gmail.com>
2025-09-16 21:45:54 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Replace this with your actual repo name on GitHub
-const repoName = '/Dezyn-aesthetix/'
-
 export default defineConfig({
-  base: `/${repoName}/`,
+  base: '/', // ✅ custom domain = root
   plugins: [react()],
   server: { port: 3000, open: false },
   preview: { port: 3000 }
 })
-
