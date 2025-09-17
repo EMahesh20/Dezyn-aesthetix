e05b10f4ef8a37afc0c7caf6ec65ac06330d7de7
EMahesh20 <maheshelango20@gmail.com>
2025-09-17 13:45:02 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/', // ✅ custom domain = root
+  base: './', // ✅ custom domain = root
   plugins: [react()],
   server: { port: 3000, open: false },
   preview: { port: 3000 }
