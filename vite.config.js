584abef13dc7074880e6ce5075abe8606e00b815
EMahesh20 <maheshelango20@gmail.com>
2025-09-17 14:10:57 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Dezyn-aesthetix/', // <- your repo name
   plugins: [react()],
+  base: '/',  // For Cloudflare Pages, root path is fine
 });
