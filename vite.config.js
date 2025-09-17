da9738398e340e532fe6157bc160d217b748b060
EMahesh20 <maheshelango20@gmail.com>
2025-09-17 14:19:18 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',  // For Cloudflare Pages, root path is fine
-});
+  base: './', // very important for static hosting
+})
